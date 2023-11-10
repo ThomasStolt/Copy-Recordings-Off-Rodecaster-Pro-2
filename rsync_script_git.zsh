b4c55f5751a8cd0fb66f788622acfe8635c2712c
Thomas Stolt <thomas.stolt@gmail.com>
2023-11-10 22:03:24 +0100

    Added the magic

---
+++ rsync_script_git.zsh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+# Check if an argument is provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <local_directory_path>"
+    exit 1
+fi
+
+# Local directory path
+LOCAL_DIR="$1"
+
+# Remote details
+REMOTE_USER_HOST="root@<your_RCP2_IP_address"
+REMOTE_DIR="/Application/sd-card-mount/RODECaster"
+
+# Rsync command
+/opt/homebrew/bin/rsync -avh --progress "$REMOTE_USER_HOST":"$REMOTE_DIR/" "$LOCAL_DIR" --info=progress2
+
+echo "Sync complete."
+
